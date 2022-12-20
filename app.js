beafc8dd5d62942afdcf2e577954cc83ada80585
JP Douma <jpdouma@gmail.com>
2022-12-20 04:49:04 +0300

    fix tied scenario ln 38

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ function getComputerChoice() {
 
 function playRound() {
   let computerSelection = getComputerChoice();
-  let playerSelection = Window.prompt("Enter Rock, Paper, or Scissors", "rock");
+  let playerSelection = prompt("Enter Rock, Paper, or Scissors", "rock");
   const rules = {
     rock: 1,
     paper: 2,
@@ -17,25 +17,25 @@ function playRound() {
     four: "It's a tie!"
   }
 
-  if (rules[playerSelection.toLowerCase()] === computerSelection) return {description: rules.four, winner: null};
-  if (rules[playerSelection.toLowerCase()] === 1 && computerSelection === 2 ) return {description: rules.three, winner: false};
-  if (rules[playerSelection.toLowerCase()] === 1 && computerSelection === 3 ) return {description: rules.one, winner: true};
-  if (rules[playerSelection.toLowerCase()] === 2 && computerSelection === 1 ) return {description: rules.three, winner: true};
-  if (rules[playerSelection.toLowerCase()] === 2 && computerSelection === 3 ) return {description: rules.two, winner: false};
-  if (rules[playerSelection.toLowerCase()] === 3 && computerSelection === 1 ) return {description: rules.one, winner: false};
-  if (rules[playerSelection.toLowerCase()] === 3 && computerSelection === 2 ) return {description: rules.two, winner: true};
+  if (rules[playerSelection.toLowerCase()] === computerSelection) return { description: rules.four, winner: null };
+  if (rules[playerSelection.toLowerCase()] === 1 && computerSelection === 2) return { description: rules.three, winner: false };
+  if (rules[playerSelection.toLowerCase()] === 1 && computerSelection === 3) return { description: rules.one, winner: true };
+  if (rules[playerSelection.toLowerCase()] === 2 && computerSelection === 1) return { description: rules.three, winner: true };
+  if (rules[playerSelection.toLowerCase()] === 2 && computerSelection === 3) return { description: rules.two, winner: false };
+  if (rules[playerSelection.toLowerCase()] === 3 && computerSelection === 1) return { description: rules.one, winner: false };
+  if (rules[playerSelection.toLowerCase()] === 3 && computerSelection === 2) return { description: rules.two, winner: true };
 }
 
-function game () {
+function game() {
   let playerScore = 0;
   let computerScore = 0;
   for (let i = 0; i < 5; i++) {
     let result = playRound();
-    if (result.winner === true) {playerScore +=1;}
-    if (result.winner === false) {computerScore +=1};
-    console.log(result.description, result.winner === true? "you won" : result.winner === false? "you lost" : "try again");
+    if (result.winner === true) { playerScore += 1; }
+    if (result.winner === false) { computerScore += 1 };
+    console.log(result.description, result.winner === true ? "you won" : result.winner === false ? "you lost" : "try again");
   }
-  return console.log(`You got ${playerScore} points, and the computer got ${computerScore} points. That means you ${playerScore > computerScore? "won!" : "lost :-(."}`);
+  return console.log(`You got ${playerScore} points, and the computer got ${computerScore} points. That means you ${playerScore > computerScore ? "won!" : playerScore === computerScore ? "tied." : "lost :-(."}`);
 
 }
 
